4fd0d4e4028ebca33516e363bc1a96ae23fe6d71
Alikh <zalikhandr@gmail.com>
2026-01-29 23:09:54 +0500

    Practice01

---
+++ PP2tasks/practice01/python-basics/strings.py
@@ -0,0 +1,19 @@
+#1
+print("Hello")
+print('Hello')
+#2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#3
+a = "Pshey"
+print(a)
+#4
+a = "Hello, World!"
+print(a[1])
+#5
+for x in "banana":
+  print(x)
+#6
+a = "Hello, World!"
+print(len(a))
